8a6103d63a7bf8f9cebb9b2b470d6f998818118d
Rafał Krupiński <krupinski.rafal@gmail.com>
2017-10-13 08:23:08 +0200

    Bump ALL THE THINGS (yarnbump)

--- src/components/root/root.jsx
+++ src/components/root/root.jsx
@@ -10,7 +10,7 @@ const PrettyBtn = Btn.extend`
 export default class Root extends Component {
   foo() { // eslint-disable-line class-methods-use-this
     import(/* webpackChunkName: 'batman' */ 'foo')
-        .then(foo => console.log(foo.default())); // eslint-disable-line no-console
+      .then(foo => console.log(foo.default())); // eslint-disable-line no-console
   }
 
   render() {
